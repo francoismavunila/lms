4fa5ae1ea54281d166132823c703aae52c12d87f
Francois Mavunila <mavunila002@gmail.com>
2025-01-15 18:16:32 +0200

    added models and schemas for books

--- app/models/book.py
+++ app/models/book.py
@@ -4,14 +4,15 @@ from sqlalchemy.ext.declarative import declarative_base
 Base = declarative_base()
 
 class Book(Base):
-    __tablename__ = 'books'
-    
+    __tablename__ = "books"
+
     id = Column(Integer, primary_key=True, index=True)
     title = Column(String(255), nullable=False)
     author = Column(String(255), nullable=False)
-    ISBN = Column(String(13), nullable=False, unique=True)
-    genre = Column(String(50), nullable=False)
-    department = Column(String(50), nullable=False)
+    isbn = Column(String(13), unique=True, nullable=False)
+    genre = Column(String(50), nullable=True)
+    department = Column(String(50), nullable=True)
     description = Column(Text, nullable=True)
     copies_available = Column(Integer, default=1)
+
     
---
+++ app/schemas/book.py
@@ -0,0 +1,32 @@
+from pydantic import BaseModel
+from typing import Optional
+
+class BookBase(BaseModel):
+    title: str
+    author: str
+    isbn: str
+    genre: Optional[str] = None
+    department: Optional[str] = None
+    description: Optional[str] = None
+    
+class BookCreate(BookBase):
+    copies_available: Optional[str] = None
+   
+    
+class BookRead(BookBase):
+    id: int
+    copies_available: int
+    
+    class Config:
+        orm_mode = True
+
+class BookUpdate(BaseModel):
+    title: Optional[str] = None
+    author: Optional[str] = None
+    isbn: Optional[str] = None
+    genre: Optional[str] = None
+    department: Optional[str] = None
+    description: Optional[str] = None
+    copies_available: Optional[str] = None
+    
+    
